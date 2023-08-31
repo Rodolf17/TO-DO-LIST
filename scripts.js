a49a4b4c6ebf4abd9087053a228b6495ebcd0c61
Rodolfo Nascimento Rodrigues <rodolfonr86@gmail.com>
2023-08-31 12:48:16 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const button = document.querySelector(".button-add-task");
+const input = document.querySelector(".input-task");
+const listaCompleta = document.querySelector(".list-tasks");
+
+
+let minhaListaDeItens = [];
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  });
+
+  input.value= "";
+
+  mostrarTarefas();
+}
+
+function mostrarTarefas() {
+  let novaLi = "";
+
+  minhaListaDeItens.forEach((item, index) => {
+    novaLi = novaLi + `
+        <li class="task ${item.concluida && "done"}" >
+            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${index})" />
+            <p>${item.tarefa}</p>
+            <img src="./img/trash.png" alt="tarefa-para-lixo" onclick="deletarItem(${index})" />
+        </li>     
+        `;
+  });
+
+    listaCompleta.innerHTML = novaLi;
+    
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens));
+}
+
+function concluirTarefa(index){
+    minhaListaDeItens[index].concluida = !minhaListaDeItens[index].concluida  
+    mostrarTarefas();
+}
+
+function deletarItem(index){
+    minhaListaDeItens.splice(index, 1);
+    mostrarTarefas();
+}
+
+function recarregarTarefas(){
+    const tarefaDoLocalStorage = localStorage.getItem('lista');
+
+    if(tarefaDoLocalStorage){
+        minhaListaDeItens = JSON.parse(tarefaDoLocalStorage);
+    
+    }
+    
+    mostrarTarefas();
+}
+
+recarregarTarefas();
+button.addEventListener("click", adicionarNovaTarefa);
